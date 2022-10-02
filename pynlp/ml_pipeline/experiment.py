e434bbb11ae981f13b5b0beef63f4ddefca7f9e7
tgroenewegen <thomorka@hotmail.com>
2022-10-02 20:26:02 +0200

    --

--- pynlp/ml_pipeline/experiment.py
+++ pynlp/ml_pipeline/experiment.py
@@ -1,6 +1,7 @@
 import logging
 import sys
-
+import seaborn as sns
+import scipy.stats as st
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
@@ -21,14 +22,22 @@ logger.addHandler(handler)
 
 
 def plot(x, y, title, yline=None):
-    plt.plot(x, y, label='Augmented data')
+    error = []
+    data = []
+    for i in range(len(x)):
+        data.append(np.mean(y[i]))
+        error.append(st.t.interval(alpha=0.95, df=len(y[i])-1, loc=np.mean(y[i]), scale=st.sem(y[i])))
+    err = np.array(error).T
+    errr4 = 2*(err - [data,data])
+    plt.errorbar(x, data, errr4[0], color='tab:green', ecolor='tab:blue',
+             capsize=3, linewidth=1, label='mean with error bars')
     plt.title(f'Impact of percentage added data on {title}')
     plt.xlabel('Percentage hate of added data')
     plt.ylabel(title)
     if yline:
         plt.plot(np.array([min(x), max(x)]), np.array([yline, yline]), color='gold', label='Unaugmented data')
         plt.legend()
-    plt.savefig(f'./results/figs/{title}.png')
+    plt.savefig(f'/Users/thomo/OneDrive/Documenten/GitHub/ma-course-subjectivity-mining/pynlp/results/figs/{title}.png')
     plt.clf()
 
 
@@ -40,7 +49,7 @@ def get_sample(xs: np.array, ys: np.array, percentage: float, n: int):
     Returns subset as tuple of (Xs, ys)
     """
     ones = ys[ys == 'hate']
-    zeros = ys[ys == 'nothate']
+    zeros = ys[ys == 'noHate']
     ones_i_sample = ones.sample(int(n*percentage), replace=False).index
     zeros_i_sample = zeros.sample(int(n*(1-percentage)), replace=False).index
 
@@ -62,17 +71,31 @@ def p_runner(pipeline_name, init_train_X, init_train_y, etrain_X, etrain_y, test
     num_to_add = max(len(init_train_y), int(percentage*len(init_train_y))) # add constant 50% of original data size
     p = min(percentage, 1)
     print(f'>> Running training with percentage: {p}, n: {num_to_add}')
-    etrain_X_to_add, etrain_y_to_add = get_sample(etrain_X, etrain_y, p, num_to_add)
-    train_y = pd.concat([init_train_y, etrain_y_to_add])
-    train_X = pd.concat([init_train_X, etrain_X_to_add])
-    results = fit_and_eval(pipeline_name, train_X, train_y, test_X, test_y)
-    with open(f'./results/tables/res_{percentage}.json', 'w') as f:
-        f.write(dumps(results))
-    
-    return (results['accuracy'], 
-        results['macro avg']['precision'], 
-        results['macro avg']['recall'], 
-        results['macro avg']['f1-score'])
+    repeats = 1
+    res_acc = []
+    res_pre = []
+    res_rec = []
+    res_f1s = []
+    for i in range(repeats):
+        etrain_X_to_add, etrain_y_to_add = get_sample(etrain_X, etrain_y, p, num_to_add)
+        train_y = pd.concat([init_train_y, etrain_y_to_add])
+        train_X = pd.concat([init_train_X, etrain_X_to_add])
+        results = fit_and_eval(pipeline_name, train_X, train_y, test_X, test_y, p)
+        res_acc.append(results['accuracy'] )
+        res_pre.append(results['macro avg']['precision'] )
+        res_rec.append(results['macro avg']['recall'] )
+        res_f1s.append(results['macro avg']['f1-score'] )
+        #with open(f'./results/tables/res_{percentage} {i}.json', 'w') as f:
+        #    f.write(dumps(results))
+
+    return (res_acc, res_pre, res_rec ,res_f1s)
+    ##return (np.mean(res_acc), np.mean(res_pre), np.mean(res_rec), np.mean(res_f1s))
+    #print(f'{percentage} + {np.mean(res_acc)}')
+
+    #return (results['accuracy'], 
+    #   results['macro avg']['precision'], 
+    #   results['macro avg']['recall'], 
+    #   results['macro avg']['f1-score'])
 
 def my_run(task_name, data_dir, pipeline_name, print_predictions):
     """
@@ -86,8 +109,10 @@ def my_run(task_name, data_dir, pipeline_name, print_predictions):
 
     percentages = np.arange(0, 1.75, .25)
     extra_vua = vf.VuaFormat()
-    extra_vua.load(f'/Users/ryansaeta/Desktop/Vrije Universiteit/Y2/S1P1/Subjectivity Mining/ma-course-subjectivity-mining/pynlp/data/extra/')
+    extra_vua.load(f'/Users/thomo/OneDrive/Documenten/GitHub/ma-course-subjectivity-mining/pynlp/data/extra/')
     etrain_X, etrain_y = extra_vua.train_instances()
+
+    etrain_y = etrain_y.map({'hate': 'hate', "nothate": "noHate"})
 
     print('>> Running initial baseline with no added data')
     logger.info('>> retrieving train/data instances...')
@@ -96,17 +121,18 @@ def my_run(task_name, data_dir, pipeline_name, print_predictions):
         # remap the y's to just 2 levels so that extra data can be incorporated
         init_train_y = init_train_y.map({'OAG': 'hate', 'CAG': 'hate', 'NAG': 'nothate'})
         test_y = test_y.map({'OAG': 'hate', 'CAG': 'hate', 'NAG': 'nothate'})
-    init_results = fit_and_eval(pipeline_name, init_train_X, init_train_y, test_X, test_y)
+    init_results = fit_and_eval(pipeline_name, init_train_X, init_train_y, test_X, test_y, 'none')
     pargs = [(pipeline_name, init_train_X, init_train_y, etrain_X, etrain_y, test_X, test_y, p) for p in percentages]
     with Pool(cpu_count()) as p:
         expresults = p.starmap(p_runner, pargs)
-    breakpoint()
     accuracies, precisions, recalls, f1s = zip(*expresults)
+
+
+
     plot(percentages, accuracies, 'accuracy', yline=init_results['accuracy'])
     plot(percentages, precisions, 'Macro Avg precision', yline=init_results['macro avg']['precision'])
     plot(percentages, recalls, 'Macro Avg recall', yline=init_results['macro avg']['recall'])
     plot(percentages, f1s, 'Macro Avg F1', yline=init_results['macro avg']['f1-score'])
-
 
 def run(task_name, data_dir, pipeline_name, print_predictions, add_extra_training=False):
     logger.info('>> Running {} experiment'.format(task_name))
@@ -121,7 +147,7 @@ def run(task_name, data_dir, pipeline_name, print_predictions, add_extra_trainin
         test_y = test_y.map({'OAG': 'hate', 'CAG': 'hate', 'NAG': 'nothate'})
     if add_extra_training:
         extra_vua = vf.VuaFormat()
-        extra_vua.load(f'/Users/ryansaeta/Desktop/Vrije Universiteit/Y2/S1P1/Subjectivity Mining/ma-course-subjectivity-mining/pynlp/data/extra/')
+        extra_vua.load(f'/Users/thomo/OneDrive/Documenten/GitHub/ma-course-subjectivity-mining/pynlp/data/extra')
         etrain_X, etrain_y = extra_vua.train_instances()
 
         train_y = pd.concat([train_y, etrain_y])
@@ -152,7 +178,7 @@ def run(task_name, data_dir, pipeline_name, print_predictions, add_extra_trainin
     return utils.eval(test_y, sys_y, output_dict=True)
 
 
-def fit_and_eval(pipeline_name, train_X, train_y, test_X, test_y):
+def fit_and_eval(pipeline_name, train_X, train_y, test_X, test_y,percentage):
     if pipeline_name.startswith('cnn'):
         pipe = cnn(pipeline_name)
         train_X, train_y, test_X, test_y = pipe.encode(train_X, train_y, test_X, test_y)
@@ -170,6 +196,15 @@ def fit_and_eval(pipeline_name, train_X, train_y, test_X, test_y):
     logger.info('>> evaluation')
     results = utils.eval(test_y, sys_y, output_dict=True)
     logger.info(utils.eval(test_y, sys_y))
+    test_X_ref = test_X
+    logger.info('>> predictions')
+    df=open(f'Balanc {percentage}.csv','w', encoding='utf-8')
+    for i in range(0, len(sys_y)):
+        df.write("{}\t{}\t{}".format(sys_y[i], test_y.values[i], test_X[i]))
+        df.write('\n')
+    df.close()
+
+    #utils.print_all_predictions(test_X_ref, test_y, sys_y, logger)
 
     return results
 
@@ -209,5 +244,5 @@ def pipeline(name):
 
 
 if __name__ == '__main__':
-    my_run('vua_format', './data/trac2018_VUA/', 'svm_libsvc_counts', False)
+    my_run('vua_format', './data/', 'svm_libsvc_counts', False)
 
